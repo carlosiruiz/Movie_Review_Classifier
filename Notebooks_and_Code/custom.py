b93d2be96f6bcd5ec9923bd97fb2fdad9cb86489
gabbyflabby <gabby1031@icloud.com>
2020-05-25 15:29:14 -0400

    pos/neg visualizations

--- Notebooks_and_Code/custom.py
+++ Notebooks_and_Code/custom.py
@@ -15,10 +15,18 @@ from sklearn.naive_bayes import MultinomialNB
 from sklearn.preprocessing import PolynomialFeatures
 import itertools
 
-def plot_confusion_matrix(cm, classes, normalize=False, title='Confusion Matrix', cmap=plt.cm.Blues):
+def plot_confusion_matrix(cm, classes, normalize=False, title='Confusion Matrix', 
+    cmap=plt.cm.Blues):
     """
     This function prints and plots the confusion matrix.
     Normalization can be applied by setting `normalize=True`.
+
+    Parameters:
+    cm -- Confusion matrix to plot; Confusion Matrix
+    classes -- Classes of the confusion matrix; List of Strings
+    normalize -- Wheteher to apply normalization or not; Bool, default = False
+    title -- Title of plot; String, default = "Confusion Matrix"
+    cmap -- Colormap to apply to confusion matrix; Colormap, default = plt.cm.Blues
     """
     if normalize:
         cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
@@ -37,17 +45,55 @@ def plot_confusion_matrix(cm, classes, normalize=False, title='Confusion Matrix'
     thresh = cm.max() / 2.
     for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
         if i == 0:
-            plt.text(j-0.1, i+0.3, format(cm[i, j], fmt), color="white" if cm[i, j] > thresh else "black")
+            plt.text(j-0.1, i+0.3, format(cm[i, j], fmt), 
+                color="white" if cm[i, j] > thresh else "black")
         if i == 1:
-            plt.text(j-0.1, i-0.2, format(cm[i, j], fmt), color="white" if cm[i, j] > thresh else "black")
+            plt.text(j-0.1, i-0.2, format(cm[i, j], fmt), 
+                color="white" if cm[i, j] > thresh else "black")
 
     plt.tight_layout()
     plt.ylabel('True Label', weight='bold')
     plt.xlabel('Predicted Label', weight='bold')
     plt.show()
 
-def classification_models(X,y,models,test_size = .2,random_state = 13,params=None,grid=False,param_grid=None,cv=5):
-    '''Creates models from given input and returns a table of evaluation metrics for each model.'''
+def classification_models(X, y, models, test_size = 0.2, random_state = 13, 
+    params=None, grid=False, param_grid=None, cv=5):
+    """
+    Creates models from given data and returns a table of evaluation metrics for 
+    each model.
+
+    Parameters:
+    X -- Features of data; Lists, numpy arrays, scipy-sparse matrices or pandas 
+    dataframes
+    y -- Target of data; Lists, numpy arrays, scipy-sparse matrices or pandas 
+    dataframes
+    models -- Models which should be run on given data; List of strings
+    Accepted strings are 'logistic', 'knn', 'tree', 'rf', 'xgb', 'AdaBoost', 
+    'GrdBoost', 'svc', and 'Bayes'.
+    test_size -- If float, should be between 0.0 and 1.0 and represent the 
+    proportion of the dataset to include in the test split. If int, represents 
+    the absolute number of test samples. If None, the value is set to the 
+    complement of the train size. If train_size is also None, it will be set to 
+    0.25; Float or Int, default = 0.2
+    random_state -- Controls the shuffling applied to the data before applying 
+    the split. Pass an int for reproducible output across multiple function 
+    calls; int or RandomState instance, default = 13
+    params -- Parameters to pass to use with given models; Dict, default = None
+    grid -- Whether to use gridsearch or not; Bool, default = False
+    param_grid -- Dictionary with parameters names (str) as keys and lists of 
+    parameter settings to try as values, or a list of such dictionaries, in 
+    which case the grids spanned by each dictionary in the list are explored. 
+    This enables searching over any sequence of parameter settings; Dict or 
+    List of Dictionaries, default = None
+    cv -- Determines the cross-validation splitting strategy. Possible inputs 
+    for cv are: None (to use the default 5-fold cross validation), integer (to 
+    specify the number of folds in a (Stratified)KFold), CV splitter, an iterable 
+    yielding (train, test) splits as arrays of indices; Int, Cross-validation 
+    Generator or an Iterable, default = 5
+
+    Returns:
+    summary_df -- Table of evaluation metric values for each model ran.
+    """
     #dictionary of models
     model_dict = {'logistic': LogisticRegression,'knn': KNeighborsClassifier,
                  'tree': DecisionTreeClassifier,'rf': RandomForestClassifier,
@@ -55,9 +101,11 @@ def classification_models(X,y,models,test_size = .2,random_state = 13,params=Non
                  'GrdBoost': GradientBoostingClassifier, 'svc': SVC,
                  'Bayes': MultinomialNB}
     #split data into train and test
-    X_train, X_test, y_train, y_test = train_test_split(X, y,test_size = test_size, random_state=random_state)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = test_size, 
+        random_state=random_state)
     #create evaluation metrics table
-    summary_df = pd.DataFrame(columns=['Model','Accuracy', 'Recall', 'Precision',  'F1'])
+    summary_df = pd.DataFrame(columns=['Model','Accuracy', 'Recall', 'Precision', 
+        'F1'])
     #variable to keep track of which parameter dictionary we are on 
     i = 0
     #if no params given and gridsearch is false, create base models
@@ -73,13 +121,15 @@ def classification_models(X,y,models,test_size = .2,random_state = 13,params=Non
                        'Accuracy': metrics.accuracy_score(y_test, predictions),
                        'Recall': metrics.recall_score(y_test, predictions),
                        'Precision': metrics.precision_score(y_test, predictions), 
-                       'F1': metrics.f1_score(y_test, predictions)},ignore_index=True)
+                       'F1': metrics.f1_score(y_test, predictions)}, 
+                       ignore_index=True)
     #if gridsearch is true, run a gridsearch and use best parameters to create models
     elif grid == True:
         for model in models:
             for key, value in model_dict.items(): 
                 if model == key: 
-                    model_use = GridSearchCV(estimator = value(),param_grid = param_grid[i],cv = cv)
+                    model_use = GridSearchCV(estimator = value(), 
+                        param_grid = param_grid[i], cv = cv)
                     model_use.fit(X_train,y_train)
                     model_grid = value(**model_use.best_params_)
                     model_grid.fit(X_train,y_train)
@@ -89,7 +139,7 @@ def classification_models(X,y,models,test_size = .2,random_state = 13,params=Non
                    'Accuracy': metrics.accuracy_score(y_test, y_pred),
                    'Recall': metrics.recall_score(y_test, y_pred),
                    'Precision': metrics.precision_score(y_test, y_pred), 
-                   'F1': metrics.f1_score(y_test, y_pred)},ignore_index=True)
+                   'F1': metrics.f1_score(y_test, y_pred)}, ignore_index=True)
                     print(f"{model}'s best parameters are {model_use.best_params_}")
     #if previous conditions are false, then we have been given parameters and gridsearch is false
     #we create models with given parameters
