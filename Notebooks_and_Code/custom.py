4c94b6e57a5489d7764d6d68d931e5e9c3d5524e
gabbyflabby <gabby1031@icloud.com>
2020-05-27 13:22:51 -0400

    EDA redone with TFIDF

--- Notebooks_and_Code/custom.py
+++ Notebooks_and_Code/custom.py
@@ -15,6 +15,116 @@ from sklearn.naive_bayes import MultinomialNB
 from sklearn.preprocessing import PolynomialFeatures
 import itertools
 
+def top_tfidf_feats(row, features, top_n=25):
+    """
+    Get top n tfidf values in row and return them with their corresponding feature names.
+
+    Parameters:
+    row -- Index for particular row being inspected; Array_like
+    features -- All words in TF-IDF's vocabulary; List of Strings
+    top_n -- Number of words to be returned; Int, default = 25
+
+    Returns:
+    df -- Pandas DataFrame with words themselves (feature names) and their
+    corresponding score.
+    """
+    # produce the indices that would order the row by tf-idf value
+    # reverse them (into descending order), and select the first top_n
+    topn_ids = np.argsort(row)[::-1][:top_n]
+
+    top_feats = [(features[i], row[i]) for i in topn_ids]
+    df = pd.DataFrame(top_feats)
+    df.columns = ['Feature', 'TF-IDF']
+    return df
+
+def top_mean_feats(Xtr, features, grp_ids=None, min_tfidf=0.1, top_n=25):
+    """
+    Return the top n features that on average are most important amongst 
+    documents in rows indentified by indices in grp_ids.
+
+    Parameters:
+    Xtr -- Tf-idf-weighted document-term matrix; 
+    sparse matrix of (n_samples, n_features)
+    features -- All words in TF-IDF's vocabulary; List of Strings
+    grp_ids -- Row indices of particular documents we want to inspect. ‘None’ 
+    indicates that we’re interested in all documents; List of Integers, default = 'None'
+    min_tfidf -- Threshold TF-IDF score; Float, default = 0.1
+    top_n -- Number of words to be returned; Int, default = 25
+
+    Returns:
+    df -- Pandas DataFrame with words themselves (feature names) and their
+    corresponding score.
+    """
+    if grp_ids:
+        D = Xtr[grp_ids].toarray()
+    else:
+        D = Xtr.toarray()
+
+    # filter out the words with relatively low scores
+    # (smaller than the provided threshold)
+    D[D < min_tfidf] = 0
+    # calculate the mean of each column across the selected rows
+    # results in a single row of tf-idf values
+    tfidf_means = np.mean(D, axis=0)
+    return top_tfidf_feats(tfidf_means, features, top_n)
+
+def top_feats_by_class(Xtr, y, features, min_tfidf=0.1, top_n=25):
+    """
+    Return a list of dfs, where each df holds top_n features and their mean 
+    tfidf value calculated across documents with the same class label.
+
+    Parameters:
+    Xtr -- Tf-idf-weighted document-term matrix; 
+    sparse matrix of (n_samples, n_features)
+    y -- Target of data; Lists, numpy arrays, scipy-sparse matrices or pandas 
+    dataframes
+    features -- All words in TF-IDF's vocabulary; List of Strings
+    min_tfidf -- Threshold TF-IDF score; Float, default = 0.1
+    top_n -- Number of words to be returned; Int, default = 25
+
+    Returns:
+    dfs -- DataFrames for each document class, each containing the top n features;
+    List of DataFrames
+    """
+    dfs = []
+    labels = np.unique(y)
+    for label in labels:
+        ids = np.where(y==label)
+        feats_df = top_mean_feats(Xtr, features, ids, min_tfidf=min_tfidf, top_n=top_n)
+        feats_df.label = label
+        dfs.append(feats_df)
+    return dfs
+
+def plot_tfidf_classfeats_h(dfs):
+    """
+    Plot the data frames returned by the function plot_tfidf_classfeats().
+
+    Parameters:
+    dfs -- DataFrames for each document class, each containing the top n features;
+    List of DataFrames
+    """
+    fig = plt.figure(figsize=(12, 9), facecolor="w")
+    x = np.arange(len(dfs[0]))
+    for i, df in enumerate(dfs):
+        ax = fig.add_subplot(1, len(dfs), i+1)
+        ax.spines["top"].set_visible(False)
+        ax.spines["right"].set_visible(False)
+        ax.set_frame_on(False)
+        ax.get_xaxis().tick_bottom()
+        ax.get_yaxis().tick_left()
+        ax.set_xlabel("Mean Tf-Idf Score", labelpad=16, fontsize=14)
+        ax.set_title("label = " + str(df.label), fontsize=16)
+        ax.ticklabel_format(axis='x', style='sci', scilimits=(-2,2))
+        ax.barh(x, df['TF-IDF'], align='center', color='#eb9b34')
+        ax.set_yticks(x)
+        ax.set_ylim([-1, x[-1]+1])
+        yticks = ax.set_yticklabels(df['Feature'])
+        plt.subplots_adjust(bottom=0.09, right=0.97, left=0.15, top=0.95, wspace=0.52)
+    plt.savefig(f'../Images/top_words_per_class.png')
+
+## Functions above taken from following blog post:
+## https://buhrmann.github.io/tfidf-analysis.html
+
 def plot_confusion_matrix(cm, classes, normalize=False, title='Confusion Matrix', 
     cmap=plt.cm.Blues):
     """
